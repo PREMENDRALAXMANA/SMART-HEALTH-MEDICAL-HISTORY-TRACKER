39a25d3c9065e0914e0225d86c9018a4e96581fa
PREMENDRALAXMANA <premtanakala2007@gmail.com>
2026-04-26 21:30:53 +0530

    stage9_numpy.py

---
+++ stage9_numpy.py
@@ -0,0 +1,11 @@
+# analysis/vital_trends.py
+
+import numpy as np
+
+ages = [20, 25, 30, 35]
+
+arr = np.array(ages)
+
+print("Average Age:", np.mean(arr))
+print("Max Age:", np.max(arr))
+print("Min Age:", np.min(arr))
